6c2cc998460f5142a6cdcb41a5f55e96b3eb0805
Damon <damonjhastings@gmail.com>
2018-12-17 09:51:47 -0800

    Style updates

--- js/app.js
+++ js/app.js
@@ -77,6 +77,7 @@
 
 
 		$('.trigger-youtube-modal').on('click', function(e){
+			debugger;
 			var id = $(e.currentTarget).parents(".youtube-item").data('id');
 			var $modal = $(e.currentTarget).parents('.spark-position-list').find('.youtube-modal');
 			$modal.find('iframe').removeAttr('src').attr('src', 'https://www.youtube.com/embed/'+ id +'?autoplay=1');
