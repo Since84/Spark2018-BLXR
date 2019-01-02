3e231494c460de7fdae40665251a2becaefbba6c
Damon <damonjhastings@gmail.com>
2019-01-01 23:18:00 -0800

    contact page

--- page-contact.php
+++ page-contact.php
@@ -1,20 +1,9 @@
 <?php
-// var_dump('here');
 get_header();
 
 $context = Timber::get_context();
 $context['page'] = new TimberPost();
-$context['contact_form'] = do_shortcode( "[contact-form-7 id='159' title='Contact Us']");
-
-
-if ( $context['page']->post_name !== "team" ){
-	$headingContext = array(
-		"layout"=> array( "heading"=>$context['page'] ),
-		"class" => "page-heading"
-	);
-	Timber::render('view/content/section_heading.html.twig', $headingContext);
-}
-
+$context['contact_form'] = do_shortcode( "[contact-form-7 id='231' title='Contact Us']");
 
 Timber::render('view/page/contact.html.twig', $context);
 
