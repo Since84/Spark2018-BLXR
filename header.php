d1cedf271bac0c52e559f0999eb5a852872914e1
Damon Hastings <damonjhastings@gmail.com>
2019-01-08 15:43:59 -0800

    header socials, spacing updates

--- header.php
+++ header.php
@@ -7,4 +7,6 @@ wp_head();
 $context = Timber::get_context();
 $context['site_logo'] = get_field('logo', 'option');
 $context['menu'] = new Timber\Menu('main-nav');
+$context['social'] = get_field('social_accounts', 'option');
+
 Timber::render('/view/nav/basic.html.twig', $context);
